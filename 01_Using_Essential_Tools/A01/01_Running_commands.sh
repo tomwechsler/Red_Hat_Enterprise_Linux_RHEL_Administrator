2b191ca41804f0fafaa62b3715d05aa0d9cf109a
Tom Wechsler <tom.wechsler@outlook.com>
2023-04-04 07:12:55 +0200

    lines edited

---
+++ 01_Using_Essential_Tools/A01/01_Running_commands.sh
@@ -0,0 +1,10 @@
+ls # command only will list current directory 
+
+ls -a # command with an option to list all file 
+
+ls -l /etc # command with the option for a long listing and an argument 
+
+
+
+Linux is case-sensitive, commands, options and arguments need to be typed in
+the correct case. Linux commands, such as ls may take options or arguments.
--- 01_Using_Essential_Tools/A01/Links.md
+++
@@ -1,6 +0,0 @@
-# Some helpful links
-
-## Red Hat
-https://www.redhat.com/en
-
-https://developers.redhat.com/
