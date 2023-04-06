5ac669992ef3dd23fed4cafa7b75dcd62cf28544
Tom Wechsler <tom.wechsler@outlook.com>
2023-04-06 06:58:00 +0200

    files edited

---
+++ 01_Using_Essential_Tools/A02/02_Executing_commands.sh
@@ -0,0 +1,14 @@
+
+ip addr help
+
+man ip
+
+man man
+
+ls /usr/share/doc
+
+
+
+Some commands have simple help options whist most have man or manual
+pages. This includes the manual page for the command man itself. Other
+documentation can exist below the /usr/share/doc directory.
