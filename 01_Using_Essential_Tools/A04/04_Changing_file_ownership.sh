e52426eb46ec970a520d3ce2d314626a632f3d1f
Tom Wechsler <tom.wechsler@outlook.com>
2023-04-10 07:02:36 +0200

    new files

---
+++ 01_Using_Essential_Tools/A04/04_Changing_file_ownership.sh
@@ -0,0 +1,24 @@
+#Working on the rhel system
+
+ls
+
+ls -l file1
+
+id
+
+sudo chown root file1
+
+ls -l file1
+
+sudo chown root:root file1
+
+ls -l file1
+
+#This set's user and group vagrant
+sudo chown vagrant: file1
+
+ls -l file1
+
+sudo chgrp root file1
+
+ls -l file1
