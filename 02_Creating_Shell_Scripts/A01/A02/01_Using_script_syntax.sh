de2e84ffac1095188fd3bb8c134eaa4e743687ca
Tom Wechsler <tom.wechsler@outlook.com>
2023-04-12 06:48:32 +0200

    new files

---
+++ 02_Creating_Shell_Scripts/A01/A02/01_Using_script_syntax.sh
@@ -0,0 +1,10 @@
+
+
+for m in hi salut ciao ; do echo $m; done
+i=3 ; while [ $i -gt 0 ]; do echo $i; let i-=1; done
+
+
+
+Traditional script syntax can be used directly at the command line, syntax is not
+restricted only to scripts. Scripts can only contain items that can be processed at
+the CLI.
