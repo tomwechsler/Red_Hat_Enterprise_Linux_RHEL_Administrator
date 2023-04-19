69098bc0b90a73205f63b629eece3dd667530c64
Tom Wechsler <tom.wechsler@outlook.com>
2023-04-19 15:09:47 +0200

    lines edited

--- 03_Operating_Running_Systems/A03/02_Editing_boot_process.sh
+++ 03_Operating_Running_Systems/A03/02_Editing_boot_process.sh
@@ -2,7 +2,7 @@
 
 #Boot Process
 #1 - BIOS/UEFI => The system bios/uefi is used to start the system and locate the boot partition
-#2 - GRUB => The boot loader in RHEL is GRUB and allows you to selevt the Linux jernel to load
+#2 - GRUB => The boot loader in RHEL is GRUB and allows you to selevt the Linux kernel to load
 #3 - Kernel => The kernel is loaded but prior to this the initialization ram dis is loaded to customize the boot process to your hardware
 
 #Shutdown the redhat system and => Start in "Normal Start"
