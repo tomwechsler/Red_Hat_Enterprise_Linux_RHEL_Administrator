a28025c4dd64356085e7c34b3077119e50069a34
Tom Wechsler <tom.wechsler@outlook.com>
2023-04-21 14:37:04 +0200

    new files

--- 03_Operating_Running_Systems/A05/02_Process_nice_renice.sh
+++ 03_Operating_Running_Systems/A05/02_Process_nice_renice.sh
@@ -1,12 +1,49 @@
 #Working on the rhel system
 
+sleep 5
+
+sleep 5&
+
+jobs
+
+#About 17 minutes
+sleep 1000&
+
+#You don't believe me
+expr 1000 / 60
+
+sudo dnf install -y bc
+
+echo "1000/60" | bc -l
+
 sleep 1000&
 
 jobs
 
+ps -elf | grep sleep
+
+pgrep sleep
+
 ps -lp $(pgrep sleep)
 
+pkill sleep
+
+nice -n 12 sleep 1000&
+
+ps -lp 9781
+
+renice 19 9781
+
+renice 10 9781
 
 
 The command ps with the option -l lists both the nice value and priority of a
 process.
+
+The command nice can be used to adjust the starting nice value, affecting the
+allocated priority. Standard users cannot set negative nice values meaning, they
+can only assign processes with a lower priority than would be assigned by
+default.
+
+The renice command can be run against existing processes. Standard users
+cannot set lower nice values than currently assigned.
---
+++ 03_Operating_Running_Systems/A05/03_Working_ps_tuned-adm.sh
@@ -0,0 +1,57 @@
+#Working on the rhel system
+
+ps
+
+sh
+
+sudo -i
+
+ps
+
+exit
+
+exit
+
+ps -elf
+
+ps -elf | grep bash
+
+ps -elf | grep thump
+
+#Open a second ssh session on first session
+who
+
+ps -elf | grep bash
+
+pgrep bash
+
+pgrep ba
+
+pgrep -l ba
+
+pgrep -l bash
+
+echo $$
+
+#The second ssh session will close
+kill 14515
+
+kill -l
+
+tuned-adm active 
+
+tuned-adm recommend
+
+tuned-adm list
+
+sudo tuned-adm profile desktop 
+
+tuned-adm active
+
+sudo tuned-adm profile virtual-guest
+
+
+
+
+We have tuning profiles in RHEL and these can be used to
+established pre-set optimizations.
