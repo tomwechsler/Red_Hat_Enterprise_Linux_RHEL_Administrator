c161b67d097924b2d71181ef8360f491cfde0d3e
Tom Wechsler <tom.wechsler@outlook.com>
2023-08-20 17:34:18 +0200

    lines edited

--- 04_Configuring_Local_Storage/A02/02_Mounting_ISO_files.sh
+++ 04_Configuring_Local_Storage/A02/02_Mounting_ISO_files.sh
@@ -2,17 +2,31 @@
 
 lsblk
 
+losetup
+
+sudo !!
+
+sudo dnf install -y wget
+
 wget https://download.virtualbox.org/virtualbox/6.0.0/VBoxGuestAdditions_6.0.0.iso
 
 sudo losetup -f VBoxGuestAdditions_6.0.0.iso 
+
+losetup
 
 lsblk /dev/loop? 
 
 sudo mount /dev/loop0 /mnt 
 
+lsblk /dev/loop0
+
 ls /mnt 
 
 sudo umount /mnt
+
+losetup
+
+lsblk
 
 modinfo loop
 
