fd76a4f4c929cbf2c29ef0831a05136349f36702
Tom Wechsler <tom.wechsler@outlook.com>
2023-08-23 06:47:40 +0200

    lines edited

--- 04_Configuring_Local_Storage/A04/04_Creating_LVM2_systems.sh
+++ 04_Configuring_Local_Storage/A04/04_Creating_LVM2_systems.sh
@@ -1,8 +1,42 @@
 #It is easier to work as root
 sudo -i
 
-sudo lvcreate -n lv1 -L 100M vg1
+pvs
 
-sudo lvcreate -n lv2 -l 12 vg1 
+pvcreate /dev/loop1p2
 
-sudo lvs vg1
+pvs
+
+vgcreate -v  vg1/dev/loop1p2
+
+vgdisplay vg1
+
+vgremove vg1
+
+pvremove /dev/loop1p2
+
+pvcreate --TABTAB /dev/loop1p2
+
+pvcreate --pvmetadatacopies 2 /dev/loop1p2
+
+vgcreate -v  vg1/dev/loop1p2
+
+vgdisplay vg1
+
+vgremove vg1
+
+pvremove /dev/loop1p2
+
+pvs
+
+vgcreate -v -s 8m vg1 /dev/loop1p2
+
+vgdisplay vg1
+
+lvs vg1
+
+lvcreate -n lv1 -L 100M vg1
+
+lvcreate -n lv2 -l 13 vg1 
+
+lvs vg1
