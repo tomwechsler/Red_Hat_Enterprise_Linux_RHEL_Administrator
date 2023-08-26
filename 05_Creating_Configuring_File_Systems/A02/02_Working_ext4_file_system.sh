cd30e7ba04742a6cb6d64ec2f1353ae7e36e6c0c
Tom Wechsler <tom.wechsler@outlook.com>
2023-08-26 06:55:23 +0200

    new file

---
+++ 05_Creating_Configuring_File_Systems/A02/02_Working_ext4_file_system.sh
@@ -0,0 +1,3 @@
+#ext4 - Check Interval - The interval in days after which fsck will be run on the filesystem.
+#ext4 - Maximum Mount Count - The number of times the filesystem can be mounted before a fsck is performed.
+
---
+++ 05_Creating_Configuring_File_Systems/A02/03_Troubleshooting_ext4.sh
@@ -0,0 +1,15 @@
+sudo dd if=/dev/urandom of=/dev/sdb2 bs=1k seek=10 count=4k
+
+lsblk
+
+dump2fs /dev/sde2 | grep uperblock #(everything is ok with /dev/sde2 - I write "uperblock" because I don't know if it's upper or lower case)
+
+dump2fs /dev/sdb2 | grep uperblock
+
+fsck /dev/sdb2 #We could try to repair the partition directly => BUT, we are looking for the superblocks
+
+mkfs.ext4 -n /dev/sdb2 #(ATTENTION: be sure to use -n!!!!) => now we see the superblocks
+
+fsck -b 40961 /dev/sdb2 #after -b insert a superblock from above
+
+fsck /dev/sdb2
