4c247c18dc912471bacf13c46da3f4c90e1cfc47
Tom Wechsler <tom.wechsler@outlook.com>
2023-08-26 06:56:05 +0200

    lines edited

--- 05_Creating_Configuring_File_Systems/A02/01_Managing_XFS_filesystems.sh
+++ 05_Creating_Configuring_File_Systems/A02/01_Managing_XFS_filesystems.sh
@@ -4,3 +4,21 @@ lsblk
 
 lsblk --help
 
+lsblk -f
+
+xfs_info /dev/sda1
+
+xfs_admin -l /dev/sda1
+
+ls /boot
+
+umount /boot
+
+xfs_admin -L "boot" /dev/sda1
+
+xfs_admin -l /dev/sda1
+
+mount /boot
+
+xfs_admin -u /dev/sda1
+
