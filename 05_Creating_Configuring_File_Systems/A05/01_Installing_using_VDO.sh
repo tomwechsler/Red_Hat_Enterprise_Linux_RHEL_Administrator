dbe44472cc5b946dde6801aa241e204266248830
Tom Wechsler <tom.wechsler@outlook.com>
2023-09-09 16:06:42 +0200

    Lines Edited

--- 05_Creating_Configuring_File_Systems/A05/01_Installing_using_VDO.sh
+++ 05_Creating_Configuring_File_Systems/A05/01_Installing_using_VDO.sh
@@ -13,13 +13,9 @@ sudo pvcreate /dev/sdX
 #Create a volume group
 sudo vgcreate myvg /dev/sdX
 
-<<<<<<< HEAD
 #Create a VDO drive
 #Replace 100G with the desired logical size of the VDO drive
 sudo lvcreate --type vdo -L 40G -n mylv myvg
-=======
-vdo create --name=vdo1 --device=/dev/sda --vdoLogicalSize=40G
->>>>>>> 351a1fdc558a5974aa79c676ba3f7763ebed4e59
 
 #Format the VDO drive with a file system of your choice (e.g. xfs)
 sudo mkfs.xfs -K /dev/myvg/mylv
