2860232be4f74f0f0268a2573d43351a44e92181
Tom Wechsler <tom.wechsler@outlook.com>
2023-10-06 15:13:21 +0200

    lines edited

--- 06_Configuring_maintaining_systems/A04/03_Kernel_args.sh
+++ 06_Configuring_maintaining_systems/A04/03_Kernel_args.sh
@@ -12,7 +12,7 @@ grubby --update-kernel=ALL --args="systemd.unit=graphical.target"
 grubby --info=ALL
 
 #Remove the systemd.unit=graphical.target argument from all boot entries
-grubby --update-kernel=ALL --remove --args="systemd.unit=graphical.target"
+grubby --update-kernel=ALL --remove-args="systemd.unit=graphical.target"
 
 #Display information about all boot entries again to verify the changes
 grubby --info=ALL
