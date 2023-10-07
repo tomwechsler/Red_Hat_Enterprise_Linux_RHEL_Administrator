db8d6b6991479f21160a18afafe346a79f301323
Tom Wechsler <tom.wechsler@outlook.com>
2023-10-07 13:27:26 +0200

    new files

--- 06_Configuring_maintaining_systems/A05/02_Working_with_cron.sh
+++ 06_Configuring_maintaining_systems/A05/02_Working_with_cron.sh
@@ -2,17 +2,40 @@
 
 sudo -i
 
+#List the files in the '/etc/cron*' directory
 ls /etc/cron*
 
+#Display the contents of the '/etc/crontab' file
 cat /etc/crontab
 
+#Schedule a job to run the 'ls /etc' command at 7:15 AM every day
 echo "15 7 * * * root ls /etc > /tmp/etc" > /etc/cron.d/ls_etc
 
+#Display the manual page for the 'crontab' command
 man crontab
 
+#Display the manual page for the 'crontab' file format
 man 5 crontab
 
-exit
+#Exit the shell
 
+#Edit the current user's crontab file
 crontab -e
 
+#Install the 'nano' text editor
+sudo install -y nano
+
+#Edit the current user's crontab file using the 'nano' editor
+EDITOR=nano crontab -e
+
+#Schedule a job to run the 'echo "Hello World"' command every 5 minutes
+*/5 * * * * echo "Hello World" > /tmp/hello
+
+#The default editor will be used
+crontab -e
+
+#List the current user's scheduled jobs
+crontab -l
+
+#Remove the current user's scheduled jobs
+crontab -r
---
+++ 06_Configuring_maintaining_systems/A05/03_Using_systemd_timer.sh
@@ -0,0 +1,24 @@
+#Working on the redhat system
+
+sudo -i
+
+#Display the manual page for the 'systemd.timer' file format
+man 5 systemd.timer
+
+#List all installed timer units
+systemctl list-unit-files --type=timer
+
+#Check for available updates using 'yum'
+yum check-update
+
+#Display the first two lines of the output of 'yum check-update'
+yum check-update | head -n2
+
+#Display the contents of the 'dnf-makecache.timer' unit file
+systemctl cat dnf-makecache.timer
+
+#Display the status of the 'dnf-makecache.service' unit
+systemctl status dnf-makecache.service
+
+#List all active timers and their next run time
+systemctl list-timers
