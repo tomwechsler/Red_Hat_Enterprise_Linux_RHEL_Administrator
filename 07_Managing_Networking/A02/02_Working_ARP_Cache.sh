719dab500ff9cead777f7d37eb31e9b2c58b022b
Tom Wechsler <tom.wechsler@outlook.com>
2023-10-24 06:53:42 +0200

    new files

---
+++ 07_Managing_Networking/A02/02_Working_ARP_Cache.sh
@@ -0,0 +1,23 @@
+#Working on the rhel system
+
+ping -c 3 192.168.56.108
+
+ip neighbor show
+
+ip n
+
+sudo ip neibhor delete 192.168.56.108 dev enp0s8 lladdr 08:00:27:4a:1c:4c
+
+ip n
+
+ping -c 3 192.168.56.108
+
+ip n
+
+ping 1.1
+
+ip n
+
+ping www.cisco.com
+
+ip n
