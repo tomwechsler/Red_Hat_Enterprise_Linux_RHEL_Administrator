1d6091f9aa2652eb67a8e89ebb87ceb83e57951f
Tom Wechsler <tom.wechsler@outlook.com>
2023-10-24 16:01:19 +0200

    lines edited

--- 07_Managing_Networking/A02/02_Working_ARP_Cache.sh
+++ 07_Managing_Networking/A02/02_Working_ARP_Cache.sh
@@ -2,10 +2,12 @@
 
 ping -c 3 192.168.56.108
 
+#Show the arp cache
 ip neighbor show
 
 ip n
 
+#Delete the arp cache entry
 sudo ip neibhor delete 192.168.56.108 dev eth1 lladdr 08:00:27:4a:1c:4c
 
 ip n
