927c0159bdec352a023f8521c2447af4aaaecf48
Tom Wechsler <tom.wechsler@outlook.com>
2023-10-28 16:10:13 +0200

    lines edited

--- 07_Managing_Networking/A03/01_Network_config_files.sh
+++ 07_Managing_Networking/A03/01_Network_config_files.sh
@@ -6,6 +6,9 @@ ip addr sh
 #List the network config files (a new path in rhel 9)
 ls /etc/NetworkManager/system-connections/
 
+#In older RHEL Systems
+ls /etc/sysconfig/network-scripts/
+
 #Display the config file
 cat /etc/NetworkManager/system-connections/enp0s8.nmconnection
 
