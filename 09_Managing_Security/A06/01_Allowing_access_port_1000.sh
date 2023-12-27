7316db8caf18a5b90d337ad4973a9c3dd9591ceb
Tom Wechsler <tom.wechsler@outlook.com>
2023-12-27 07:09:00 +0100

    new files

---
+++ 09_Managing_Security/A06/01_Allowing_access_port_1000.sh
@@ -0,0 +1,49 @@
+#Working on the alma system
+
+#Switch to the root user and simulate a full login shell
+sudo -i
+
+#Install the 'httpd' package using dnf package manager
+dnf install -y httpd
+
+#Stop the 'httpd' service
+systemctl stop httpd
+
+#Display the line containing 'Listen 80' in the '/etc/httpd/conf/httpd.conf' file
+grep 'Listen 80' /etc/httpd/conf/httpd.conf
+
+#List all SELinux network port contexts
+semanage port -l
+
+#List all SELinux network port contexts and filter for 'http'
+semanage port -l | grep http
+
+#Replace 'Listen 80' with 'Listen 1000' in the '/etc/httpd/conf/httpd.conf' file
+sed -Ei 's/^(Listen) 80/\1 1000/' /etc/httpd/conf/httpd.conf
+
+#Display the line containing 'Listen 80' in the '/etc/httpd/conf/httpd.conf' file
+grep 'Listen 80' /etc/httpd/conf/httpd.conf
+
+#Display the line containing 'Listen 1000' in the '/etc/httpd/conf/httpd.conf' file
+grep 'Listen 1000' /etc/httpd/conf/httpd.conf
+
+#Start the 'httpd' service
+systemctl start httpd
+
+#Search the audit logs for AVC messages
+ausearch -m AVC -ts recent
+
+#Display the lines containing 'sealert' in the '/var/log/messages' file
+grep sealert /var/log/messages
+
+#Display the SELinux alert with the specified identifier
+sealert -l 2a8b1e4e-2b1e-4b1a-8b1e-4b1a2e1b2a1e
+
+#Add a new SELinux network port context for TCP port 1000 with 'http_port_t' type
+semanage port -a -t http_port_t -p tcp 1000
+
+#Start the 'httpd' service
+systemctl start httpd
+
+#Display the listening sockets
+ss -ntl
