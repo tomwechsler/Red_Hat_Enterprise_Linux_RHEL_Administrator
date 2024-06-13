7b114e7519edc2a68281adca437b5c11a04ca75b
Tom Wechsler <tom.wechsler@outlook.com>
2024-06-13 14:54:44 +0200

    lines edited

--- 04_Configuring_Local_Storage/A02/01_Listing_block_device.sh
+++ 04_Configuring_Local_Storage/A02/01_Listing_block_device.sh
@@ -1,23 +1,31 @@
 #Working on rhel
 
-lsTABTAB
+#List the files and directories in the current directory
+ls
 
+#Display information about the CPU architecture
 lscpu
 
+#Show the status of modules in the Linux Kernel
 lsmod
 
-modeinfo sd_mod | less
+#Display information about a Linux Kernel module
+modinfo sd_mod | less
 
+#List all block devices
 lsblk
 
-lslbk /dev/sda
+#Display block device information for /dev/sda
+lsblk /dev/sda
 
-lslbk /dev/sda2
+#Display block device information for /dev/sda2
+lsblk /dev/sda2
 
-#What is above sda2 is part of sda
-lslbk -s /dev/sda2
+#Display the hierarchy of block devices, showing what block devices are 'above' /dev/sda2 (i.e., what /dev/sda2 is a part of)
+lsblk -s /dev/sda2
 
+#Print the block device (partition and filesystem) UUIDs
 sudo blkid
 
+#Display file system disk space usage with human-readable sizes (-h) and print the file system type (-T)
 df -Th
-
