428d0aaed273164e21c77b92a07dfd3ca5b80d6f
Tom Wechsler <tom.wechsler@outlook.com>
2024-06-23 13:51:06 +0200

    lines edited

--- 04_Configuring_Local_Storage/A03/05_Partitioning_disks_parted.sh
+++ 04_Configuring_Local_Storage/A03/05_Partitioning_disks_parted.sh
@@ -6,6 +6,8 @@ sudo parted /dev/loop1 mklabel msdos
 #Create a new partition
 sudo parted /dev/loop1 mkpart primary 0% 25%
 
+#Print the partition table
 sudo parted /dev/loop1 print
 
+#List all block devices
 lsblk
