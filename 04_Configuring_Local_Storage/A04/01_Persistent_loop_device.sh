65abd7d1aed4e7a387c922f936f0e766f2b24f0b
Tom Wechsler <tom.wechsler@outlook.com>
2024-06-26 17:58:31 +0200

    lines edited

--- 04_Configuring_Local_Storage/A04/01_Persistent_loop_device.sh
+++ 04_Configuring_Local_Storage/A04/01_Persistent_loop_device.sh
@@ -1,31 +1,46 @@
+#List all block devices to check the current setup
 lsblk
 
+#Reboot the system immediately
 sudo reboot
 
+#List all block devices again to verify changes after reboot
 lsblk
 
+#List the contents of /var/disks to check for disk1
 ls /var/disks
 
+#Associate the first available loop device with /var/disks/disk1
 sudo losetup -f /var/disks/disk1
 
+#List all block devices to verify the loop device setup
 lsblk
 
+#Inform the system of partition table changes on /dev/loop0
 sudo partprobe /dev/loop0
 
+#Mount the filesystem with UUID=8a3f1d99-bcf2-42bd-b96a-8cefae231ab8 to /data
 sudo mount UUID=8a3f1d99-bcf2-42bd-b96a-8cefae231ab8 /data
 
+#Attempt to mount a filesystem of type XFS without specifying a device or mount point, which will fail
 mount -t XFS
 
+#Open the losetup.service file for editing or create it if it doesn't exist
 sudo systemctl edit --full --force losetup.service
 
 #Use the losetup.service.txt file
 
+#Reload the systemd manager configuration
 sudo systemctl daemon-reload
 
+#Enable losetup.service to start at boot
 sudo systemctl enable losetup.service
 
+#Reboot the system immediately
 sudo reboot
 
+#List all block devices to verify changes after reboot
 lsblk
 
+#Mount the filesystem with UUID=8a3f1d99-bcf2-42bd-b96a-8cefae231ab8 to /data
 sudo mount UUID=8a3f1d99-bcf2-42bd-b96a-8cefae231ab8 /data
