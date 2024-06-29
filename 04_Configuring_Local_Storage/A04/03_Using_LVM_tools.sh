10d0f611890614645411ad2df1a754f408136d31
Tom Wechsler <tom.wechsler@outlook.com>
2024-06-29 07:03:01 +0200

    lines edited

--- 04_Configuring_Local_Storage/A04/03_Using_LVM_tools.sh
+++ 04_Configuring_Local_Storage/A04/03_Using_LVM_tools.sh
@@ -1,13 +1,19 @@
 #Managing LVM Storage (Physical Volumes, Volume Groups, and Logical Volumes)
 
+#Lists all Physical Volumes (PVs) on the system
 sudo pvs
 
+#Intended for shell auto-completion, showing commands related to 'pv'
 pv TABTAB
 
+#Lists all Volume Groups (VGs) on the system
 sudo vgs
 
+#Displays detailed information about all Volume Groups
 sudo vgdisplay
 
+#Lists all Logical Volumes (LVs) in all Volume Groups
 sudo lvs
 
+#Displays detailed information about all Logical Volumes
 sudo lvdisplay
