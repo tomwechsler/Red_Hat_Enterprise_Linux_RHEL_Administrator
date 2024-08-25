dc37a2e633d5fba8f08de55b92f13577007dbfaa
Tom Wechsler <tom.wechsler@outlook.com>
2024-08-25 16:21:48 +0200

    lines edited

--- 05_Creating_Configuring_File_Systems/A02/04_Securing_mount_points.sh
+++ 05_Creating_Configuring_File_Systems/A02/04_Securing_mount_points.sh
@@ -1,35 +1,47 @@
 #Working on the rhel system
 
+#Switch to the root user
 sudo -i
 
+#Create the directory /my/sales and any necessary parent directories
 mkdir -p /my/sales
 
+#Change the permissions of /my/sales to 707 (rwx---rwx) and display a message for each file processed
 chmod -v 707 /my/sales
 
+#Change the current directory to /my/sales
 cd /my/sales
 
 #Leave the root shell
 exit
 
+#Change the current directory to /my/sales
 cd /my/sales
 
+#Change the current directory to the home directory
 cd
 
+#Switch to the root user
 sudo -i
 
+#Change the permissions of /my/sales to 700 (rwx------) and display a message for each file processed
 chmod -v 700 /my/sales
 
+#Mount the filesystem on /dev/sda to /my/sales
 mount /dev/sda /my/sales
 
+#Change the permissions of /my/sales to 707 (rwx---rwx) and display a message for each file processed
 chmod -v 707 /my/sales
 
 #Leave the root shell
 exit
 
+#change into my/sales
 cd /my/sales
 
 cd
 
+#Unmount the /my/sales filesystem
 sudo umount /my/sales
 
 #Permission denied
