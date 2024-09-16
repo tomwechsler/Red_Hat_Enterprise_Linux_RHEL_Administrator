fda78fc273d125653201e44f5f1394b19b143ca4
Tom Wechsler <tom.wechsler@outlook.com>
2024-09-16 16:38:36 +0200

    lines edited

--- 05_Creating_Configuring_File_Systems/A03/02_Using_sticky_bit_prevent_deletions.sh
+++ 05_Creating_Configuring_File_Systems/A03/02_Using_sticky_bit_prevent_deletions.sh
@@ -16,14 +16,19 @@ ls -ld /tmp
 #Create a directory and change the perms/group
 sudo mkdir /admin
 
+#Change the group ownership of the /admin directory to 'wheel'
 sudo chgrp wheel /admin
 
+#List the details of the /admin directory, including permissions and group ownership
 ls -ld /admin
 
+#Remove all permissions for others on the /admin directory and display a message for each file processed
 sudo chmod -v o= /admin
 
+#List the details of the /admin directory again to verify the changes
 ls -ld /admin
 
+#Add write permission for the group on the /admin directory and display a message for each file processed
 sudo chmod -v g+w /admin
 
 #Now lets create some files
