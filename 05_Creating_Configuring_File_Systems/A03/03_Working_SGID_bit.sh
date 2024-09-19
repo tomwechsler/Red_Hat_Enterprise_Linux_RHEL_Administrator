617130cee3dd96ef264e69db17038108f5037609
Tom Wechsler <tom.wechsler@outlook.com>
2024-09-19 13:01:39 +0200

    lines edited

--- 05_Creating_Configuring_File_Systems/A03/03_Working_SGID_bit.sh
+++ 05_Creating_Configuring_File_Systems/A03/03_Working_SGID_bit.sh
@@ -1,27 +1,39 @@
 #Working on the rhel system
 
+#Change the permissions of the /admin directory to 1770 (rwxrwx--T) and display a message for each file processed
 sudo chmod -v 1770 /admin
 
+#List the details of the /admin directory, including permissions and special bits
 ls -ld /admin
 
+#Switch to the user 'tux'
 sudo su - tux
 
+#Display the user ID and group ID information for the current user
 id
 
+#Change the current directory to /admin
 cd /admin
 
+#Exit the current shell session (log out from the 'tux' user)
 exit
 
+#List the files and directories in the current directory with detailed information
 ls -l
 
+#Set the default file creation permissions to 007 (rw-rw----)
 umask 007
 
+#Create an empty file named 'file1'
 touch file1
 
+#List the details of 'file1' to verify its permissions
 ls -l file1
 
+#Change the group ID on execution (SGID) bit of the /admin directory and display a message for each file processed
 sudo chmod -v g+s /admin
 
+#Create an empty file named 'file2'
 touch file2
 
 #The file2 is created with the group of the parent directory
