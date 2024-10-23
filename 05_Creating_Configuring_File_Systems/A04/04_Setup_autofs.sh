634d7ace18b138ae2572c6b8adae3e342b3d82a9
Tom Wechsler <tom.wechsler@outlook.com>
2024-10-23 06:40:39 +0200

    line edited

--- 05_Creating_Configuring_File_Systems/A04/04_Setup_autofs.sh
+++ 05_Creating_Configuring_File_Systems/A04/04_Setup_autofs.sh
@@ -2,19 +2,28 @@
 
 sudo -i
 
+#Install the autofs package
 dnf install -y autofs
 
-vim /etc/autoTABTAB
+#Open the /etc/auto.master file in the vim editor to configure autofs
+vim /etc/auto.master
 
+#Open the /etc/auto.master.d/srv.autofs file in the vim editor to add autofs configuration for /srv
 vim /etc/auto.master.d/srv.autofs
 
+#Add the autofs map file /etc/auto.data for the /srv directory
 /srv /etc/auto.data
 
+#List the contents of the /srv directory
 ls /srv
 
+#Open the /etc/auto.data file in the vim editor to configure NFS mount points
 vim /etc/auto.data
+
+#Add an entry to mount the NFS export /srv/nfsdata from 192.168.56.108 with read-write and soft mount options
 nfsdata -rw,soft 192.168.56.108:/srv/nfsdata
 
+#Enable and start the autofs service immediately
 systemctl enable --now autofs
 
 #Exit the root session
