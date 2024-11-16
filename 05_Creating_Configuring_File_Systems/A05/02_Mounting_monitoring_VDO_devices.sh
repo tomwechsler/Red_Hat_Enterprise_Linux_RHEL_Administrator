9c55d42165ac4afb5f44cf7193aac822b03bb492
Tom Wechsler <tom.wechsler@outlook.com>
2024-11-16 14:17:02 +0100

    line edited

--- 05_Creating_Configuring_File_Systems/A05/02_Mounting_monitoring_VDO_devices.sh
+++ 05_Creating_Configuring_File_Systems/A05/02_Mounting_monitoring_VDO_devices.sh
@@ -13,26 +13,38 @@ sudo mount /dev/myvg/mylv /my/vdo
 #Add the VDO drive to the /etc/fstab file so it is automatically mounted at boot time
 echo '/dev/mapper/myvg-mylv /my/vdo xfs defaults,_netdev 0 0' | sudo tee -a /etc/fstab
 
+#Mount all filesystems defined in /etc/fstab
 mount -a
 
+#Attempt to mount a filesystem of type XFS without specifying a device or mount point, which will fail
 mount -t xfs
 
+#Change the permissions of /my/vdo to 3770 (rwxrws--T) and display a message for each file processed
 chmod -v 3770 /my/vdo
 
+#Display VDO statistics in human-readable format
 vdostats --human-readable
 
+#List the files and directories in /boot with detailed information and human-readable file sizes
 ls -lh /boot
 
+#Copy the initramfs image of the current kernel 50 times to /my/vdo with unique filenames
 for i in {1..50} ; do cp /boot/initramfs-$(uname -r).img /my/vdo/initramfs-$(uname -r).img.$i ; done
 
+#List the files and directories in /my/vdo with detailed information and human-readable file sizes
 ls -lh /my/vdo
 
+#Display the disk usage of /my/vdo in human-readable format
 du -sh /my/vdo
 
+#Display VDO statistics in human-readable format
 vdostats --human-readable
 
+#Copy the initramfs image of the current kernel 50 more times to /my/vdo with unique filenames
 for i in {1..50} ; do cp /boot/initramfs-$(uname -r).img /my/vdo/initramfs-$(uname -r)s.img.$i ; done
 
+#Display VDO statistics in human-readable format
 vdostats --human-readable
 
+#Display the disk usage of /my/vdo in human-readable format
 du -sh /my/vdo
