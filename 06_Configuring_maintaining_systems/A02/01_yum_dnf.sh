2e034935d52cc5a1eb9a88a6ebb5e81abea24dba
Tom Wechsler <tom.wechsler@outlook.com>
2024-12-22 15:07:04 +0100

    lines edited

--- 06_Configuring_maintaining_systems/A02/01_yum_dnf.sh
+++ 06_Configuring_maintaining_systems/A02/01_yum_dnf.sh
@@ -1,13 +1,19 @@
 #Working on redhat
 
+#Display the path of the 'dnf' executable
 which dnf
 
+#Display the path of the 'yum' executable
 which yum
 
+#List detailed information about the 'dnf' executable
 ls -l /usr/bin/dnf 
 
+#List detailed information about the 'yum' executable
 ls -l /usr/bin/yum
 
+#List detailed information about the 'dnf' executable using the path returned by 'which'
 ls -l $(which dnf)
 
+#List detailed information about the 'yum' executable using the path returned by 'which'
 ls -l $(which yum)
