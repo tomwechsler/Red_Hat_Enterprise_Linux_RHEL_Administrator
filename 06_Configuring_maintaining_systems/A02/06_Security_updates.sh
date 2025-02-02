b46154f3b1333ecfb88b126425ba07d63d7f8f9d
Tom Wechsler <tom.wechsler@outlook.com>
2025-02-02 08:02:05 +0100

    lines edited

--- 06_Configuring_maintaining_systems/A02/06_Security_updates.sh
+++ 06_Configuring_maintaining_systems/A02/06_Security_updates.sh
@@ -11,11 +11,20 @@ dnf updateinfo --security
 #Display information about available security updates with severity "Important"
 dnf updateinfo --sec-severity Important
 
+#Display information about available security updates with severity "Critical"
+dnf updateinfo --sec-severity Critical
+
+#Display information about available security updates with severity "Moderate"
+dnf updateinfo --sec-severity Moderate
+
+#Display information about available security updates with severity "Low"
+dnf updateinfo --sec-severity Low
+
 #Display detailed information about available security updates with severity "Important"
 dnf updateinfo info --sec-severity Important 
 
-#Install the security update with advisory RHSA-2021:0218
-dnf update --advisory RHSA-2023:4201
+#Install the security update with advisory RHSA-2025:0146
+dnf update --advisory RHSA-2025:0146
 
 #Install all available security updates with severity "Important"
 dnf update --sec-severity Important
