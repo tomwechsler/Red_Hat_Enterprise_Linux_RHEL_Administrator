827a769490f27f84267ebbf00548b06550072f4b
Tom Wechsler <tom.wechsler@outlook.com>
2025-03-05 14:49:09 +0100

    lines edited

--- 01_Using_Essential_Tools/A05/02_Using_tar.sh
+++ 01_Using_Essential_Tools/A05/02_Using_tar.sh
@@ -1,31 +1,40 @@
 #Working on the rhel system
 
+#Display the total disk usage of the /etc directory in a human-readable format
 sudo du -sh /etc
 
+#Create a tarball of the /etc directory named etc.tar
 sudo tar -cf etc.tar /etc
 
+#List detailed information about the etc.tar file
 ls -lh etc.tar
 
-#List the content
+#List the contents of the etc.tar file
 tar -tf etc.tar
 
+#Extract the contents of the etc.tar file
 tar -xf etc.tar
 
+#Change the current directory to etc
 cd etc
 
+#Print the current working directory
 pwd
 
+#List the contents of the etc directory
 ls
 
+#Change the current directory to the parent directory
 cd ..
 
-#Do NOT on a production system
+#Do NOT do this on a production system: Remove the /etc/hosts file
 sudo rm /etc/hosts
 
+#Change the current directory to the root directory
 cd /
 
+#Extract the /etc/hosts file from the etc.tar tarball located in the vagrant user's home directory
 sudo tar -xf ~vagrant/etc.tar etc/hosts
 
+#Display the contents of the /etc/hosts file to verify the restoration
 cat /etc/hosts
-
-
