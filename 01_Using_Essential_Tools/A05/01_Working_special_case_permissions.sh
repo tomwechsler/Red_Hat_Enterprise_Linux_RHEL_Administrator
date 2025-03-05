88531432a347d0faf0664961aaf3c89749732324
Tom Wechsler <tom.wechsler@outlook.com>
2025-03-05 15:00:18 +0100

    lines edited

--- 01_Using_Essential_Tools/A05/01_Working_special_case_permissions.sh
+++ 01_Using_Essential_Tools/A05/01_Working_special_case_permissions.sh
@@ -1,36 +1,46 @@
 #Working on the rhel system
 
+#Create an empty file named 'log'
 touch log
 
+#List detailed information about 'log'
 ls -l log
 
+#Display the contents of 'log'
 cat log
 
+#Change the permissions of 'log' to write-only for the user
 chmod -v u=w log
 
-#Permission denied
+#Permission denied: Attempt to display the contents of 'log'
 cat log
 
-#This will work
+#This will work: Append the string 'hello' to 'log'
 echo hello >> log
 
+#Display the contents of 'log' with superuser privileges
 sudo cat log
 
+#Create a directory named 'logs' with permissions 155 (r-xr-x--x)
 mkdir -m 155 logs
 
+#Change the current directory to 'logs'
 cd logs
 
-#Permission denied
+#Permission denied: Attempt to list the contents of 'logs'
 ls
 
+#Change the current directory to the parent directory
 cd ..
 
+#Change the permissions of 'logs' to 0355 (r-xr-xr-x) and display a message for each file processed
 chmod -v 0355 logs
 
+#Append the string 'test' to 'logs/file'
 echo test >> logs/file
 
-#If you know the paths it works
+#If you know the paths it works: Display the contents of 'logs/file'
 cat logs/file
 
+#List the contents of the 'logs' directory
 ls logs
-
