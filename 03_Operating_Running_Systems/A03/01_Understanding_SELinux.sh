5ca9ff0ca40ea94ef7d8f9b33f7d726d47523088
Tom Wechsler <tom.wechsler@outlook.com>
2025-09-01 07:07:15 +0200

    some updates

--- 03_Operating_Running_Systems/A03/01_Understanding_SELinux.sh
+++ 03_Operating_Running_Systems/A03/01_Understanding_SELinux.sh
@@ -5,22 +5,32 @@
 #Important => working as root
 sudo -i
 
+#Create a new user named 'fred' with a home directory
 useradd -m fred
 
+#List the SELinux context of the /etc/shadow file
 ls -Z /etc/shadow
 
+#Change the SELinux type of /etc/shadow to user_home_t
 chcon -t user_home_t /etc/shadow
 
+#List the SELinux context of the /etc/shadow file again to verify the change
 ls -Z /etc/shadow
 
+#Switch to the user 'fred' with a login shell
 sudo -i -u fred
 
+#Restore the default SELinux context for /etc/shadow and display verbose output
 restorecon -v /etc/shadow
 
+#Switch to the user 'fred' with a login shell again
 sudo -i -u fred
 
+#Exit the current shell session
 exit
 
+#Exit the current shell session
 exit
 
+#Switch to the root user with a login shell
 sudo -i
--- 03_Operating_Running_Systems/A03/03_Recovering_lost_root_password.sh
+++ 03_Operating_Running_Systems/A03/03_Recovering_lost_root_password.sh
@@ -1,4 +1,4 @@
-#Working on the rhel system in virtualbox
+#Working on the rhel system in virtualbox (for RHEL 10 navigate down in this document)
 
 #Boot Process
 #1 - BIOS/UEFI => The system bios/uefi is used to start the system and locate the boot partition
@@ -7,25 +7,57 @@
 
 #Edit linux entry
 
-Remove rhgb and quiet and add rd.break enforcing=0 (hit ctrl +x to start)
+#Remove 'rhgb' and 'quiet' from the kernel parameters and add 'rd.break enforcing=0' (press Ctrl + X to start)
 
-switch_root# mount -o remount,rw /sysroot
+#Remount the sysroot as read-write
+switch_root#mount -o remount,rw /sysroot
 
+#Change root into the sysroot
 chroot /sysroot
 
-sh-4.3$ passwd
+#Set a new root password
+passwd
 
-sh-4.3$ touch /.autorelabel
+#Create the /.autorelabel file to trigger SELinux relabeling on next boot
+touch /.autorelabel
 
+#Exit the chroot shell
 exit
 
-switch_root# mount -o remount,ro /sysroot
+#Remount the sysroot as read-only
+switch_root#mount -o remount,ro /sysroot
 
+#Exit the emergency shell to continue booting
 exit
 
 #Login as root and if have not create the autorelabel file then
 restorecon -v /etc/shadow
 
+#Check the current SELinux status
 getenforce
 
+#Set SELinux to enforcing mode
 setenforce 1
+
+#On RHEL 10 use
+
+#Boot Process
+#1 - BIOS/UEFI => The system bios/uefi is used to start the system and locate the boot partition
+#2 - GRUB => The boot loader in RHEL is GRUB and allows you to selevt the Linux jernel to load
+#3 - Kernel => The kernel is loaded but prior to this the initialization ram dis is loaded to customize the boot process to your hardware
+
+#Edit linux entry
+
+#Remove 'rhgb' and 'quiet' from the kernel parameters and add init=/bin/bash (press Ctrl + X to start)
+
+#Remount the root filesystem in read-write mode
+mount -o remount,rw /
+
+#Enter the passwd
+passwd
+
+#To relabel all files on next system boot
+touch /.autorelabel
+
+#Reboot the system
+exec /sbin/init
